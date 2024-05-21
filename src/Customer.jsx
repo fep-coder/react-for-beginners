febe970e986810d5339cfe612e24b616b8afb3ea
voja <voja@gmail.com>
2024-05-21 13:17:08 +0200

    13 Add Customer

--- src/Customer.jsx
+++ src/Customer.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import CustomerList from "./CustomerList";
+import AddCustomer from "./AddCustomer";
 
 function Customer() {
-    const customers = [
+    const [customers, setCustomers] = useState([
         { id: 1, name: "John", email: "john@gmail.com" },
         { id: 2, name: "Jack", email: "jack@gmail.com" },
-    ];
+    ]);
 
     return (
         <div className="row">
             <div className="col-8 mx-auto">
+                <AddCustomer setCustomers={setCustomers} />
                 <CustomerList customers={customers} />
             </div>
         </div>
