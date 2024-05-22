94ae48f54e2e3edc0c0e7a55449df47ddd87abe9
voja <voja@gmail.com>
2024-05-22 13:12:58 +0200

    17 Routes and Links

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Header() {
     return (
         <nav className="navbar navbar-expand-sm bg-body-tertiary">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">
+                <Link className="navbar-brand" to="/">
                     React
-                </a>
+                </Link>
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -19,19 +21,19 @@ function Header() {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link" href="#">
+                            <Link className="nav-link" to="/products">
                                 Products
-                            </a>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">
+                            <Link className="nav-link" to="/products/details">
                                 ProductDetails
-                            </a>
+                            </Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">
+                            <Link className="nav-link" to="/pricing">
                                 Pricing
-                            </a>
+                            </Link>
                         </li>
                     </ul>
                 </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Header from "./Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Products from "./Products";
+import ProductDetails from "./ProductDetails";
+import NotFound from "./NotFound";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Header />
+        <BrowserRouter>
+            <div className="container">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/products">
+                        <Route path="" element={<Products />} />
+                        <Route path="details" element={<ProductDetails />} />
+                    </Route>
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     </React.StrictMode>
 );
