3753abd98e78829fc8bfe535558f4da3b249a5d0
voja <voja@gmail.com>
2024-05-23 09:46:38 +0200

    20 redux toolkit and store setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Header from "./Header";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import Products from "./Products";
-import ProductDetails from "./ProductDetails";
-import NotFound from "./NotFound";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <div className="container">
-                <Header />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/products">
-                        <Route path="" element={<Products />} />
-                        <Route
-                            path="details/:name?/:id?"
-                            element={<ProductDetails />}
-                        />
-                    </Route>
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </div>
-        </BrowserRouter>
+        <Provider store={store}>
+            <div className="container">hello</div>
+        </Provider>
     </React.StrictMode>
 );
